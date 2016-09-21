8f8f08a2d9372590bb59199701e37a467d39b4eb
Dave <dmah-little@csumb.edu>
2016-09-21 19:04:48 +0000

    early commit

--- Lab3.php
+++ Lab3.php
@@ -3,7 +3,25 @@
     $suits = 4;
     $deck = array();
     //populate card array
-    for($i=0;$i<13;$i++){
-        $deck
+    for($i=0;$i<$values;$i++){
+        for($j=0;$j<$suits;$j++){
+            $deck[$i*$j + $j] = array("value"=>$i,"suit"=>$j);
+        }
     }
+    
+    //shuffle
+    shuffle($deck);
+    
+    //deal
+    for($i=0;$i<$values;$i++){
+        for($j=0;$j<$suits;$j++){
+            $deck[$i*$j + $j] = array("value"=>$i,"suit"=>$j);
+        }
+    }
+    
+    
+    print_r($deck);
+    echo "<br>";
+    shuffle($deck);
+    print_r($deck);
 ?>
